600682aa4f8278edbbca8a4bbc250997fe8ad425
john-lenci-bianchi <john.lenci.bianchi@gmail.com>
2023-12-07 20:35:23 +0100

    update dataset and graph generator

--- datasetGenerator/main.py
+++ datasetGenerator/main.py
@@ -3,7 +3,7 @@ from libraries.apiCaller import apiCaller
 import os
 
 # Delete the contents of the directory "output"
-directory_path = "datasetGenerator/output"
+directory_path = "output"
 for filename in os.listdir(directory_path):
     file_path = os.path.join(directory_path, filename)
     try:
@@ -13,10 +13,10 @@ for filename in os.listdir(directory_path):
         print(f"Error deleting files in the directory: {str(e)}")
 
 
-dates = ['10-01-2023','10-04-2023','09-07-2023','05-07-2023']
+dates = ['10-01-2023','10-10-2023','07-09-2023','07-05-2023']
 times = ['1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm', '12:00pm', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am']
 
-places = pd.read_csv('orignini/pathsConFrazioni.csv')
+places = pd.read_csv('../origini/pathsConFrazioni.csv')
 
 for i in range(len(places)):
     place = places.iloc[i]
