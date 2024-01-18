5e626251027f02ae59d2f22b108af04090b96be4
bianchi-john <john.lenci.bianchi@gmail.com>
2024-01-18 10:16:50 +0100

    switched to 2024 gtfs files

--- datasetGenerator/main.py
+++ datasetGenerator/main.py
@@ -15,7 +15,7 @@ for filename in os.listdir(directory_path):
         print(f"Error deleting files in the directory: {str(e)}")
 
 
-dates = ['10-01-2023','10-10-2023','07-09-2023','07-05-2023']
+dates = ['10-01-2024','10-10-2024','07-09-2024','07-05-2024']
 times = ['1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm', '12:00pm', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am']
 
 places = pd.read_csv('../origini/pathsConFrazioni.csv')
